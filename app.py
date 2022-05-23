6ed73cb64cc9d32c510c0645121d0f3fbec1c462
Shreyanshishere <105097099+Shreyanshishere@users.noreply.github.com>
2022-05-23 17:28:55 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import numpy as np
+from flask import Flask , render_template, request
+from backend import *
+lis=['non hate tweet','hate tweet']
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
+@app.route("/predict1",methods=['POST'])
+def predict1():
+    st=request.form['tweet']
+    res=predict(st)[0]
+    # return f"your result is "+str(lis[res]) 
+    res= "your tweet is analyzed as a "+str(lis[res])
+    return render_template('index.html',result=res)
+if __name__=="__main__":
+    app.run(debug=True)
